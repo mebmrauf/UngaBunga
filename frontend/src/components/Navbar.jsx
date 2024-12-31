b1e8d91a570e15075af5ab155ee03633d3a830d6
B M Rauf <bmrauf@outlook.com>
2025-01-01 00:47:50 +0600

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import {assets} from "../assets/frontend_assets/assets.js";
 
 import {Link, NavLink} from "react-router-dom";
-import {useState} from "react";
+import {useContext, useState} from "react";
+import {ShopContext} from "../context/ShopContext.jsx";
 
 const Navbar= () => {
 
     const [visible, setVisible] = useState(false);
+    const { setShowSearch } = useContext(ShopContext);
+
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
             <Link to='/'><img src={assets.logo} className='w-36' alt='logo' /></Link>
@@ -30,7 +33,7 @@ const Navbar= () => {
             </ul>
 
             <div className='flex items-center gap-6'>
-                <img src={assets.search_icon} className='w-5 cursor-pointer' alt='' />
+                <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt='' />
                 <div className='group relative'>
                     <img className='w-5 cursor-pointer' src={assets.profile_icon} alt='' />
                     <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
