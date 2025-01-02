fb6e9eaf8d65e24b3d3c96b4e26e3e658a15a76e
B M Rauf <bmrauf@outlook.com>
2025-01-02 23:05:37 +0600

    add and update

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,22 @@
+import Title from "../components/Title.jsx";
+import {assets} from "../assets/frontend_assets/assets.js";
+
 const About = () => {
     return (
         <div>
+            <div className="text-2xl text-center pt-8 border-t">
+                <Title text1={'About'} text2={'Us'} />
+            </div>
 
+            <div className='my-10 flex flex-col md:flex-row gap-16'>
+                <img className='w-full md:max-w-[450px]' src={assets.about_img} alt='' />
+                <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+                    <p>rjhgbhberbghrbhbrgkbkrhbkjbbkwbkjetbwjnerrjkn</p>
+                    <p>nrjkgjkrkrebjkrbkgbkgberbgbrbibje</p>
+                    <b>Our Mission</b>
+                </div>
+
+            </div>
         </div>
     )
 }
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,20 @@
+import Title from "../components/Title.jsx";
+import {assets} from "../assets/frontend_assets/assets.js";
 
 const Contact = () => {
     return (
         <div>
-
+            <div className='text-center text-2xl pt-10 border-t'>
+                <Title text1={'Contact'} text2={'Us'}/>
+            </div>
+            <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+                <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt={''} />
+                <div className='flex flex-col justify-center items-start gap-6'>
+                    <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+                    <p className='text-gray-500'>Kha 224 Pragati Sarani, Merul Badda <br/>Dhaka 1212, Bangladesh</p>
+                    <p className='text-gray-500'>Mobile: +8801305-091891 <br/>Email: raufbiswas@bmrauf.me</p>
+                </div>
+            </div>
         </div>
     )
 }
